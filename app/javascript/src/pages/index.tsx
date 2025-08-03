cd41be652e2673d540c28aa1b5d7635e1908382e
Daniel Eze <dan4allu93@gmail.com>
2025-08-03 23:49:17 +0100

    implements the ui

--- app/javascript/src/pages/index.tsx
+++ app/javascript/src/pages/index.tsx
@@ -1,37 +1,116 @@
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "flowbite-react";
-import { useState } from "react";
+import {
+	Dropdown,
+	DropdownDivider,
+	DropdownHeader,
+	DropdownItem,
+	Navbar,
+	NavbarBrand,
+	NavbarCollapse,
+	NavbarLink,
+	NavbarToggle,
+	Avatar,
+	HR,
+	Card,
+	Button,
+	ButtonGroup,
+	Tooltip,
+} from "flowbite-react";
+
+import { Trash, Pencil, Eye, Ungroup, LayoutTemplate, FileQuestionMark, Timer } from "lucide-react";
+
+import { RouteObject } from "react-router";
+import DashboardLayout from "../layouts/DashboardLayout";
+import ApplicationSidebar from "../components/Sidebar/Sidebar";
+import AssessmentCard, { AssessmentStatus } from "../components/Cards/AssessmentCard";
 
 export default function Homepage() {
-	const [openModal, setOpenModal] = useState(true);
-
 	return (
-		<>
-			<Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
-			<Modal show={openModal} onClose={() => setOpenModal(false)}>
-				<ModalHeader>Terms of Service</ModalHeader>
-				<ModalBody>
-					<div className="space-y-6">
-						<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-							With less than a month to go before the European Union enacts new consumer
-							privacy laws for its citizens, companies around the world are updating their
-							terms of service agreements to comply.
-						</p>
-						<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-							The European Union’s General Data Protection Regulation (G.D.P.R.) goes into
-							effect on May 25 and is meant to ensure a common set of data rights in the
-							European Union. It requires organizations to notify users as soon as possible
-							of high-risk data breaches that could personally affect them.
-						</p>
+		<DashboardLayout>
+			<DashboardLayout.Sidebar>
+				<ApplicationSidebar />
+			</DashboardLayout.Sidebar>
+
+			<DashboardLayout.Content>
+				<Navbar fluid className="p-0 sm:p-0">
+					<NavbarBrand>
+						<span className="self-center whitespace-nowrap text-2xl font-bold font-display dark:text-white">
+							Assessments
+						</span>
+					</NavbarBrand>
+					<div className="flex md:order-2">
+						<Dropdown
+							arrowIcon={false}
+							inline
+							label={
+								<Avatar
+									alt="User settings"
+									rounded
+									placeholderInitials="ED"
+									className="cursor-pointer hidden md:block"
+								/>
+							}
+						>
+							<DropdownHeader>
+								<span className="block text-sm">Daniel Eze</span>
+								<span className="block truncate text-sm font-medium">daniel@undp.com</span>
+							</DropdownHeader>
+							<DropdownItem>Dashboard</DropdownItem>
+							<DropdownItem>Settings</DropdownItem>
+							<DropdownDivider />
+							<DropdownItem>Sign out</DropdownItem>
+						</Dropdown>
+						<NavbarToggle />
 					</div>
-				</ModalBody>
-				<ModalFooter>
-					<Button onClick={() => setOpenModal(false)}>I accept</Button>
-					<Button color="alternative" onClick={() => setOpenModal(false)}>
-						Decline
-					</Button>
-				</ModalFooter>
-			</Modal>
-		</>
+					<NavbarCollapse className="md:hidden">
+						<NavbarLink href="#" active>
+							Home
+						</NavbarLink>
+						<NavbarLink href="#">About</NavbarLink>
+						<NavbarLink href="#">Services</NavbarLink>
+						<NavbarLink href="#">Pricing</NavbarLink>
+						<NavbarLink href="#">Contact</NavbarLink>
+					</NavbarCollapse>
+				</Navbar>
+				<hr className="my-4 h-px border-0 bg-gray-200 dark:bg-gray-700" />
+
+				<section className="flex flex-wrap gap-8 items-center">
+					<AssessmentCard
+						title="Digital Transformation Readiness"
+						description="Assessment to determine organizational readiness for digital transformation initiatives."
+						status={AssessmentStatus.ACTIVE}
+						date="July 21, 2023"
+					/>
+
+					<AssessmentCard
+						title="Business Impact Assessment"
+						description="Comprehensive assessment to evaluate the impact of business operations on local communities and environment."
+						status={AssessmentStatus.DRAFT}
+						date="July 20, 2023"
+					/>
+
+					<AssessmentCard
+						title="Digital Transformation Readiness"
+						description="Assessment to determine organizational readiness for digital transformation initiatives."
+						status={AssessmentStatus.ACTIVE}
+						date="July 21, 2023"
+					/>
+
+					<AssessmentCard
+						title="Business Impact Assessment"
+						description="Comprehensive assessment to evaluate the impact of business operations on local communities and environment."
+						status={AssessmentStatus.DRAFT}
+						date="July 20, 2023"
+					/>
+
+					<AssessmentCard
+						title="Business Impact Assessment"
+						description="Comprehensive assessment to evaluate the impact of business operations on local communities and environment."
+						status={AssessmentStatus.ACTIVE}
+						date="July 20, 2023"
+					/>
+				</section>
+			</DashboardLayout.Content>
+		</DashboardLayout>
 	);
 }
 
--- vite.config.mts
+++ vite.config.mts
@@ -1,34 +1,36 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
-import svgr from 'vite-plugin-svgr'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    RubyPlugin(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        app: './app/javascript/entrypoint/client/index.tsx'
-      }
-    }
-  },
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
-})
+	plugins: [
+		tsconfigPaths(),
+		RubyPlugin(),
+		svgr({
+			svgrOptions: {
+				icon: true,
+			},
+		}),
+		flowbiteReact(),
+	],
+	optimizeDeps: {
+		include: ["react", "react-dom"],
+	},
+	resolve: {
+		extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				app: "./app/javascript/entrypoint/client/index.tsx",
+			},
+		},
+	},
+	server: {
+		hmr: {
+			overlay: false,
+		},
+	},
+});
